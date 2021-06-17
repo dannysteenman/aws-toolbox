00c3c1b749dbeee3b8d82e4c1cd870b38f285e90
Danny Steenman <danny.steenman@icloud.com>
2021-06-17 15:51:23 +0200

    Adding Lint Action on push to any branch and fixed linting errors.

--- s3/s3_create_tar_file.py
+++ s3/s3_create_tar_file.py
@@ -7,13 +7,33 @@
 import boto3
 import tarfile
 import os.path
+
 s3Client = boto3.client("s3")
 s3object = boto3.resource("s3")
+
+
+def lambda_handler(event, context):
+    agtBucket = "angularbuildbucket"
+    key = ""
+    tar = tarfile.open("/tmp/example.tar", "w")
+    source_dir = "/tmp/"
+    for fname in get_matching_s3_keys(bucket=agtBucket, prefix=key, suffix=".js"):
+        print(fname)
+        # file_obj = s3object.Object(agtBucket, fname)
+        # file_content = file_obj.get()['Body'].read()
+        # tar.add(file_content)
+        s3object.Bucket(agtBucket).download_file(fname, "/tmp/" + fname)
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+    tar.close()
+    s3object.meta.client.upload_file(
+        source_dir + "example.tar", agtBucket, "example.tar"
+    )
 
 
 def get_matching_s3_keys(bucket, prefix="", suffix=""):
     """
     Generate the keys in an S3 bucket.
+
     :param bucket: Name of the S3 bucket.
     :param prefix: Only fetch keys that start with this prefix (optional).
     :param suffix: Only fetch keys that end with this suffix (optional).
@@ -32,18 +52,5 @@ def get_matching_s3_keys(bucket, prefix="", suffix=""):
             break
 
 
-agtBucket = "angularbuildbucket"
-
-key = ""
-
-tar = tarfile.open("/tmp/example.tar", "w")
-
-source_dir = "/tmp/"
-
-for fname in get_matching_s3_keys(bucket=agtBucket, prefix=key, suffix=".js"):
-    print(fname)
-    file_obj = s3object.Object(agtBucket, fname)
-    s3object.Bucket(agtBucket).download_file(fname, "/tmp/" + fname)
-    tar.add(source_dir, arcname=os.path.basename(source_dir))
-tar.close()
-s3object.meta.client.upload_file(source_dir + "example.tar", agtBucket, "example.tar")
+if __name__ == "__main__":
+    lambda_handler({"invokingEvent": '{"messageType":"ScheduledNotification"}'}, None)
--- s3/s3_search_file_in_bucket.py
+++ s3/s3_search_file_in_bucket.py
@@ -5,26 +5,10 @@
 import boto3
 
 client = boto3.client("s3")
-bucket_name = "shreedattatechnosolutions"
+bucket_name = "bucket_name"
 prefix = ""
 
 s3 = boto3.client("s3")
-# all_objects = s3.list_objects(Bucket = bucket_name)
-# print(all_objects)
-
-# result = client.list_objects(Bucket=bucket_name, Delimiter='/')
-# for obj in result.get('CommonPrefixes'):
-prefix = obj.get("Prefix")
-file_list = ListFiles(client, bucket_name, prefix)
-for file in file_list:
-    if "processed/files" in file:
-        print("Found", file)
-
-result = s3.list_objects(Bucket=bucket_name, Prefix="", Delimiter="")
-contents = result.get("Contents")
-for content in contents:
-    if "processed/files/" in content.get("Key"):
-        print("Do the process")
 
 
 def ListFiles(client, bucket_name, prefix):
@@ -34,5 +18,13 @@ def ListFiles(client, bucket_name, prefix):
     response = client.list_objects(Bucket=_BUCKET_NAME, Prefix=_PREFIX)
 
     for content in response.get("Contents", []):
-        print(content)
         yield content.get("Key")
+
+
+result = client.list_objects(Bucket=bucket_name, Delimiter="/")
+for obj in result.get("CommonPrefixes"):
+    prefix = obj.get("Prefix")
+    file_list = ListFiles(client, bucket_name, prefix)
+    for file in file_list:
+        if "processed/files" in file:
+            print("Found", file)
