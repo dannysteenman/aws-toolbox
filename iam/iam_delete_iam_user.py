2ab1a76c804852e36eee7ceec81084678386ec4b
Danny Steenman <danny@towardsthecloud.com>
2023-02-16 21:34:40 +0100

    fix: refactor iam scripts and fix flake8 and black format

--- iam/iam_delete_iam_user.py
+++ iam/iam_delete_iam_user.py
@@ -2,174 +2,98 @@
 #
 # This script deletes iam users
 
-import boto3
 import argparse
-
-# delete access key attach with user.
-def delete_accesskey_for_user(iam_client, user_name):
-    try:
-        response = iam_client.list_access_keys(UserName=user_name)
-        if response["AccessKeyMetadata"] != []:
-            for data in response["AccessKeyMetadata"]:
-                iam_client.delete_access_key(
-                    UserName=user_name, AccessKeyId=data["AccessKeyId"]
-                )
-            print("User Access key Deleted")
-    except Exception as e:
-        print(e)
+import boto3
 
 
-# delete Signing Certificates
+def delete_access_keys(iam_client, user_name):
+    response = iam_client.list_access_keys(UserName=user_name)
+    for access_key in response.get("AccessKeyMetadata", []):
+        iam_client.delete_access_key(
+            UserName=user_name, AccessKeyId=access_key["AccessKeyId"]
+        )
+    if response.get("AccessKeyMetadata"):
+        print("Access keys deleted.")
+
+
 def delete_signing_certificates(iam_client, user_name):
+    response = iam_client.list_signing_certificates(UserName=user_name)
+    for certificate in response.get("Certificates", []):
+        iam_client.delete_signing_certificate(
+            UserName=user_name, CertificateId=certificate["CertificateId"]
+        )
+    if response.get("Certificates"):
+        print("Signing certificates deleted.")
+
+
+def delete_login_profile(iam_client, user_name):
     try:
-        response = iam_client.list_signing_certificates(UserName=user_name)
-        if response["Certificates"] != []:
-            for data in response["Certificates"]:
-                iam_client.delete_signing_certificate(
-                    UserName=user_name, CertificateId=data["CertificateId"]
-                )
-            print("User Signing Certificates Deleted")
-    except Exception as e:
-        print(e)
+        iam_client.delete_login_profile(UserName=user_name)
+        print("Login profile deleted.")
+    except iam_client.exceptions.NoSuchEntityException:
+        print("No login profile found.")
 
 
-# delete user login profile
-def delete_user_login_profile(iam_client, user_name):
-    try:
-        response = iam_client.get_login_profile(UserName=user_name)
-        if response != {}:
-            iam_client.delete_login_profile(UserName=user_name)
-            print("User Login Profile Deleted")
-    except Exception as e:
-        print(f"No User login Profile Found: {e}")
+def delete_mfa_devices(iam_client, user_name):
+    response = iam_client.list_mfa_devices(UserName=user_name)
+    for device in response.get("MFADevices", []):
+        iam_client.deactivate_mfa_device(
+            UserName=user_name, SerialNumber=device["SerialNumber"]
+        )
+    if response.get("MFADevices"):
+        print("MFA devices deleted.")
 
 
-# Deleting 2FA Devices associated with User
-def delete_associated_fa_with_user(iam_client, user_name):
-    try:
-        response = iam_client.list_mfa_devices(UserName=user_name)
-        if response["MFADevices"] != []:
-            for data in response["MFADevices"]:
-                iam_client.deactivate_mfa_device(
-                    UserName=user_name, SerialNumber=data["SerialNumber"]
-                )
-            print("User attached MFA Devices Deleted")
-    except Exception as e:
-        print(e)
+def detach_policies(iam_client, user_name):
+    response = iam_client.list_attached_user_policies(UserName=user_name)
+    for policy in response.get("AttachedPolicies", []):
+        iam_client.detach_user_policy(UserName=user_name, PolicyArn=policy["PolicyArn"])
+    if response.get("AttachedPolicies"):
+        print("Attached policies removed.")
 
 
-# Removing Attached User Policies
-def remove_attached_policies(iam_client, user_name):
-    try:
-        response = iam_client.list_attached_user_policies(UserName=user_name)
-        if response["AttachedPolicies"] != []:
-            for data in response["AttachedPolicies"]:
-                iam_client.detach_user_policy(
-                    UserName=user_name, PolicyArn=data["PolicyArn"]
-                )
-            print("User attached Policies Removed")
-    except Exception as e:
-        print(e)
-
-
-# deleteing Inline policies
 def delete_inline_policies(iam_client, user_name):
-    try:
-        response = iam_client.list_user_policies(UserName=user_name)
-        if response["PolicyNames"] != []:
-            for policy_name in response["PolicyNames"]:
-                iam_client.delete_user_policy(
-                    UserName=user_name, PolicyName=policy_name
-                )
-            print("User attached Inline Policies Deleted")
-    except Exception as e:
-        print(e)
+    response = iam_client.list_user_policies(UserName=user_name)
+    for policy_name in response.get("PolicyNames", []):
+        iam_client.delete_user_policy(UserName=user_name, PolicyName=policy_name)
+    if response.get("PolicyNames"):
+        print("Inline policies deleted.")
 
 
-# delete user permissions boundary
 def delete_permission_boundary(iam_client, user_name):
     try:
         iam_client.delete_user_permissions_boundary(UserName=user_name)
-        print("User Permissions Boundary Deleted")
-    except Exception as e:
-        print(f"No boundary permissions attached to user: {e}")
+        print("Permissions boundary deleted.")
+    except iam_client.exceptions.NoSuchEntityException:
+        print("No permissions boundary found.")
 
 
-# remove user from group memberships
 def remove_user_from_groups(iam_client, user_name):
-    try:
-        response = iam_client.list_groups_for_user(UserName=user_name)
-        if response["Groups"] != []:
-            for data in response["Groups"]:
-                iam_client.remove_user_from_group(
-                    GroupName=data["GroupName"], UserName=user_name
-                )
-            print("User Removed from All Groups")
-    except Exception as e:
-        print(e)
-
-
-# remove ssh public key
-def remove_ssh_public_key(iam_client, user_name):
-    try:
-        response = iam_client.list_ssh_public_keys(UserName=user_name)
-        if response["SSHPublicKeys"] != []:
-            for data in response["SSHPublicKeys"]:
-                iam_client.delete_ssh_public_key(
-                    UserName=user_name, SSHPublicKeyId=data["SSHPublicKeyId"]
-                )
-            print("User Ssh Public Key Deleted")
-    except Exception as e:
-        print(e)
-
-
-# delete user from IAM
-def delete_user_from_iam(iam_client, user_name):
-    try:
-        iam_client.delete_user(UserName=user_name)
-        print("IAM User- {0} Deleted!".format(user_name))
-    except Exception as e:
-        print(e)
-
-
-# main function to delete IAM User
-def delete_iam_user(iam_client, user_name):
-    delete_accesskey_for_user(iam_client=iam_client, user_name=user_name)
-    delete_signing_certificates(iam_client=iam_client, user_name=user_name)
-    delete_user_login_profile(iam_client=iam_client, user_name=user_name)
-    delete_associated_fa_with_user(iam_client=iam_client, user_name=user_name)
-    remove_attached_policies(iam_client=iam_client, user_name=user_name)
-    delete_inline_policies(iam_client=iam_client, user_name=user_name)
-    delete_permission_boundary(iam_client=iam_client, user_name=user_name)
-    remove_ssh_public_key(iam_client=iam_client, user_name=user_name)
-    remove_user_from_groups(iam_client=iam_client, user_name=user_name)
-    delete_user_from_iam(iam_client=iam_client, user_name=user_name)
-
-
-def get_args():
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            "--user_name",
-            help="Enter IAM user name which you want to delete",
-            type=str,
-            required=True,
+    response = iam_client.list_groups_for_user(UserName=user_name)
+    for group in response.get("Groups", []):
+        iam_client.remove_user_from_group(
+            GroupName=group["GroupName"], UserName=user_name
         )
-        return parser.parse_args()
-    except Exception as e:
-        print(e)
+    if response.get("Groups"):
+        print("Removed user from groups.")
 
 
-if __name__ == "__main__":
+def delete_ssh_public_keys(iam_client, user_name):
+    response = iam_client.list_ssh_public_keys(UserName=user_name)
+    for ssh_key in response.get("SSHPublicKeys", []):
+        iam_client.delete_ssh_public_key(
+            UserName=user_name, SSHPublicKeyId=ssh_key["SSHPublicKeyId"]
+        )
+    if response.get("SSHPublicKeys"):
+        print("SSH public keys deleted.")
 
-    try:
-        iam_client = boto3.client("iam")
-        user_name = get_args().user_name
-        print(f"IAM User who will be deleted: {user_name}")
-        ans = raw_input("Do you want to delete this user? Y/N")
-        if ans.lower() == "y":
-            delete_iam_user(iam_client=iam_client, user_name=user_name)
-        else:
-            print("User not deleted")
-    except Exception as e:
-        print(e)
+
+def delete_iam_user(iam_client, user_name):
+    delete_access_keys(iam_client, user_name)
+    delete_signing_certificates(iam_client, user_name)
+    delete_login_profile(iam_client, user_name)
+    delete_mfa_devices(iam_client, user_name)
+    detach_policies(iam_client, user_name)
+    delete_inline_policies(iam_client, user_name)
+    delete_permission_boundary(iam_client, user_name)
+    remove_user_from_groups(iam_client, user_name)
--- s3/s3_delete_empty_buckets.py
+++ s3/s3_delete_empty_buckets.py
@@ -7,25 +7,25 @@
 import boto3
 
 # create an S3 client
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3")
 # create an S3 resource
 s3 = boto3.resource("s3")
 
 # list all of the S3 buckets in the account
 response = s3_client.list_buckets()
-buckets = response['Buckets']
+buckets = response["Buckets"]
 
 # filter out the empty buckets with versioning disabled
 empty_buckets = []
 for bucket in buckets:
-    bucket_name = bucket['Name']
+    bucket_name = bucket["Name"]
     result = s3_client.list_objects_v2(Bucket=bucket_name)
-    if 'Contents' not in result:
+    if "Contents" not in result:
         versioning = s3_client.get_bucket_versioning(Bucket=bucket_name)
-        if versioning.get('Status') != 'Enabled':
+        if versioning.get("Status") != "Enabled":
             empty_buckets.append(bucket_name)
 
 # delete the empty buckets
 for bucket_name in empty_buckets:
     s3.Bucket(bucket_name).delete()
-    print(f'Bucket {bucket_name} deleted.')
+    print(f"Bucket {bucket_name} deleted.")
